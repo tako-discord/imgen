cd1e47b815e768abf8fe43b62ed75341bd93a925
Pukimaa <pukima@pukima.site>
2023-08-01 15:21:43 +0000

    proper png support

--- utils.py
+++ utils.py
@@ -1,19 +1,23 @@
 import requests
 from io import BytesIO
 from typing import Tuple
-from PIL import Image, ImageOps
+from PIL import Image, ImageOps, ImageChops
 
-def get_image(url: str, size: Tuple[int, int]=(512, 512), conversion: bool = True):
+
+def get_image(url: str, size: Tuple[int, int] = (512, 512), conversion: bool = True):
     buffer = BytesIO(requests.get(url).content)
     if conversion:
         return Image.open(buffer).convert("RGBA").resize(size)
     return Image.open(buffer)
 
+
 def mask(img: Image, mask: str):
-    mask = Image.open('assets/masks/' + mask + ".png").convert('L')
+    mask = Image.open("assets/masks/" + mask + ".png").convert("L")
     output = ImageOps.fit(img, mask.size)
+    mask = ImageChops.darker(mask, img.split()[-1])
     output.putalpha(mask)
     return output.convert("RGBA")
+
 
 def valid_avatar_url(url: str):
     if url.startswith("https://cdn.discordapp.com/"):
